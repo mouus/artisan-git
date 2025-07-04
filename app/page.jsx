f8e014137554eafc60b84993e74ffc32f2065276
unknown <musannif.git@gmail.com>
2025-07-04 19:36:02 +0500

    phone what can you do section commented

--- app/page.jsx
+++ app/page.jsx
@@ -228,9 +228,9 @@ export default function Home() {
           />
         </div>
         <div>
-          <div>
+          {/* <div>
             <h2>What</h2>
-          </div>
+          </div> */}
         </div>
       </div>
 
